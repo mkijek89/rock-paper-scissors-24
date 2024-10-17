9f4fdadaad3be14bc0718998a4b97d47afda60e0
mkijek89 <misiex131@gmail.com>
2024-10-18 01:00:34 +0200

    Create function to get computer choice and prompt to get human choice

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 
 // 1. get computer choice with Math.random
 
-    function getRandom(min,max) {
-      min = Math.ceil(min)
-      max = Math.floor(max)
-        return Math.floor(Math.random()*(max-min+1))+min;
-    }
+function getRandom(min,max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
 
-console.log(getRandom(1,3));
-// if 1 then rock
-    // else if 2 paper
-    // else scissors
+function getComputerChoice() {
+    if (getRandom(1,3) === 1) {
+        return "rock";
+    } else if (getRandom(1,3) === 2) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
 // 2. get human choice from input
+
+let humanChoice = prompt("type: rock, paper or scissors");
+console.log(humanChoice);
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+
 // 3. compare choices and declare score
 // 4. play the round
 // 5. count score and end game when someone gets 5
