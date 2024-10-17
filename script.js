41611a44da7119f0e58693e59095ca788a483a64
mkijek89 <misiex131@gmail.com>
2024-10-18 00:44:03 +0200

    Create getRandom function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+
+// 1. get computer choice with Math.random
+
+    function getRandom(min,max) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+        return Math.floor(Math.random()*(max-min+1))+min;
+    }
+
+console.log(getRandom(1,3));
+// if 1 then rock
+    // else if 2 paper
+    // else scissors
+// 2. get human choice from input
+// 3. compare choices and declare score
+// 4. play the round
+// 5. count score and end game when someone gets 5
